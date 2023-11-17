ef26e2c36b760bef0ed6cbf4713d29bc04edcbfd
nourdine jazi <nourdinejazi2@gmail.com>
2023-11-17 12:36:41 +0100

    f res commit

--- app/projects/clothify/carousel.tsx
+++ app/projects/clothify/carousel.tsx
@@ -1,56 +1,89 @@
-"use client"
+"use client";
 import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
-import long_cap_clothify_big from '/public/long_cap_clothify_big.png'
+import long_cap_clothify_big from "/public/long_cap_clothify_big.png";
 
 import Link from "next/link";
 
-
-
 export default class SimpleSlider extends Component {
-    render() {
-      const settings = {
-        arrows : true,
-        dots: false,
-        infinite: true,
-        speed: 700,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      };
+  render() {
+    const settings = {
+      arrows: true,
+      dots: false,
+      infinite: true,
+      speed: 700,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
 
-      return (
-            <div className="scrollimg     ">
-          
-              <div className="   ">
-                <Slider  {...settings} className=" mt-8 mx-auto max-w-[900px]    ">
+    return (
+      <div className="scrollimg     ">
+        <div className=" mx-3   ">
+          <Slider
+            {...settings}
+            className=" mt-8 mx-auto max-w-[900px]    pb-32 "
+          >
+            <div className="h-[450px] overflow-auto rounded-xl ">
+              <Image
+                src={long_cap_clothify_big}
+                width={1000}
+                height={2000}
+                quality={100}
+                placeholder="blur"
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
 
-                <div className="h-[450px] overflow-auto rounded-xl ">
-                  <Image src={long_cap_clothify_big}  width={1000}  height={2000} quality={100} placeholder="blur"  className="  mx-auto "  alt="image"></Image>
-                </div>
-                
-                <div className="h-[450px] overflow-auto rounded-xl">
-                <Image src={"/long_cap_collection_clothify.png"}   width={1000} height={2000} quality={100}  className="  mx-auto "  alt="image"></Image>
-                </div>
+            <div className="h-[450px] overflow-auto rounded-xl">
+              <Image
+                src={"/long_cap_collection_clothify.png"}
+                width={1000}
+                height={2000}
+                quality={100}
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
 
-                <div className="h-[450px] overflow-auto rounded-xl">
-                <Image src={"/item_long_cap.png"}   width={1000} height={2000} quality={100}  className="  mx-auto "  alt="image"></Image>
-                </div>
+            <div className="h-[450px] overflow-auto rounded-xl">
+              <Image
+                src={"/item_long_cap.png"}
+                width={1000}
+                height={2000}
+                quality={100}
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
 
-                <div className="h-[450px] overflow-auto rounded-xl">
-                <Image src={"/filter_page_cap.png"}   width={1000} height={2000} quality={100}  className="  mx-auto "  alt="image"></Image>
-                </div>
+            <div className="h-[450px] overflow-auto rounded-xl">
+              <Image
+                src={"/filter_page_cap.png"}
+                width={1000}
+                height={2000}
+                quality={100}
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
 
-                <div className="h-[450px] overflow-auto rounded-xl">
-                <Image src={"/cart_cap.png"}   width={1000} height={2000} quality={100}  className="  mx-auto "  alt="image"></Image>
-                </div>
-                </Slider>
-              </div>  
-        </div> 
-          
-
-      );
-    }
+            <div className="h-[450px] overflow-auto rounded-xl">
+              <Image
+                src={"/cart_cap.png"}
+                width={1000}
+                height={2000}
+                quality={100}
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    );
   }
+}
--- app/projects/espace-bakhta/carousel.tsx
+++ app/projects/espace-bakhta/carousel.tsx
@@ -1,42 +1,56 @@
-"use client"
+"use client";
 import React, { Component } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Image from "next/image";
-import espace_home_cap from "/public/espace_home_cap.png"
-import reserv_cap from "/public/reserv_cap.png"
+import espace_home_cap from "/public/espace_home_cap.png";
+import reserv_cap from "/public/reserv_cap.png";
 import Link from "next/link";
 
-
-
-
 export default class SimpleSlider extends Component {
-    render() {
-      const settings = {
-        dots: true,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+  render() {
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 300,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
 
-      return (
-            <div className="  w-full     ">
-              <div className="   ">
-                <Slider  {...settings} className="   mt-8 mx-auto max-w-[900px] h-[550px]     ">
-                  <div className="h-[450px] overflow-auto  rounded-xl ">
-                  <Image src={espace_home_cap} placeholder="blur"  width={1000}  height={2000} quality={70}  className="  mx-auto "  alt="image"></Image>
-                  </div>
+    return (
+      <div className=" sm:mx-3       ">
+        <div className="   ">
+          <Slider
+            {...settings}
+            className="   mt-8 mx-auto max-w-[900px] h-[550px] sm:pb-32    "
+          >
+            <div className="h-[450px] overflow-auto  rounded-xl ">
+              <Image
+                src={espace_home_cap}
+                placeholder="blur"
+                width={1000}
+                height={2000}
+                quality={70}
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
 
-                  <div className="h-[450px] overflow-auto  rounded-xl ">
-                  <Image src={reserv_cap} placeholder="blur"  width={1000}  height={2000} quality={70}  className="  mx-auto "  alt="image"></Image>
-                  </div>
-                </Slider>
-              </div>  
-        </div> 
-          
-
-      );
-    }
+            <div className="h-[450px] overflow-auto  rounded-xl ">
+              <Image
+                src={reserv_cap}
+                placeholder="blur"
+                width={1000}
+                height={2000}
+                quality={70}
+                className="  mx-auto "
+                alt="image"
+              ></Image>
+            </div>
+          </Slider>
+        </div>
+      </div>
+    );
   }
+}
