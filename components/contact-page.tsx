bff08771f7b090414bb82604020cfde0cc723560
nourdine jazi <nourdinejazi2@gmail.com>
2023-11-28 22:23:56 +0100

    changed the form

--- components/contact-page.tsx
+++ components/contact-page.tsx
@@ -1,29 +1,30 @@
 import SVGComponent from "./Svg-comp";
 import WorkFrom from "./workform";
 
-
-
 const ContactPage = () => {
-    return ( 
-        <div className="diagonal3 w-full   min-h-screen flex flex-col text-center">
-                <div  className=" p-3  flex flex-col  items-center justify-center pt-10  gap-5">
-                    <div className="text-gray-400 border-2 border-gray-400 rounded-full bg-gray-400 bg-opacity-10  w-16 h-16 m-auto text-4xl text-center pt-2  ">4</div>
-                    <p className=" text-gray-400 text-3xl font-semibold mt-16 ">Contact Me.</p>
-                    <span className="text-4xl font-semibold text-white sm:text-2xl  ">Got a question or proposal, or just want to say hello? Go ahead.</span>
-                    <div className="bg-gray-400 w-[160px] h-[1px] m-auto "/>
-                    <p className="text-white sm:w-full sm:text-sm  text-base w-1/2 m-auto">
-                        Shoot me a message  if you want to connect! You can also find me on Linkedin  if that's more your speed.
-                    </p>
-                </div>
-                <WorkFrom />
-
-               <SVGComponent />
-               
+  return (
+    <div className="diagonal3 w-full   min-h-screen flex flex-col text-center">
+      <div className=" p-3  flex flex-col  items-center justify-center pt-10  gap-5">
+        <div className="text-gray-400 border-2 border-gray-400 rounded-full bg-gray-400 bg-opacity-10  w-16 h-16 m-auto text-4xl text-center pt-2  ">
+          4
         </div>
+        <p className=" text-gray-400 text-3xl font-semibold mt-16 ">
+          Contact Me.
+        </p>
+        <span className="text-4xl font-semibold text-white sm:text-2xl  ">
+          Got a question or proposal, or just want to say hello? Go ahead.
+        </span>
+        <div className="bg-gray-400 w-[160px] h-[1px] m-auto " />
+        <p className="text-white sm:w-full sm:text-sm  text-base w-1/2 m-auto">
+          Shoot me a message if you want to connect! You can also find me on
+          Linkedin if that's more your speed.
+        </p>
+      </div>
+      <WorkFrom />
 
-      
-     );
+      <SVGComponent />
+    </div>
+  );
+};
 
-}
- 
 export default ContactPage;
--- components/workform.tsx
+++ components/workform.tsx
@@ -1,75 +1,105 @@
-"use client"
-
+"use client";
 
 import { useState } from "react";
-import axios from "axios"
-import {toast} from "react-hot-toast"
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import {
+  ArrowBigRightDash,
+  ArrowRight,
+  ArrowRightFromLine,
+} from "lucide-react";
 
 interface FormDataProps {
-    name : string
-    email : string
-    message : string
-  }
-  
-const WorkFrom = () => {
-    const [name,setName]=useState("")
-    const [email,setEmail]=useState("")
-    const [message,setMessage]=useState("")
-    
-    
-    const onSubmit = async (ev: React.FormEvent) => {
-        ev.preventDefault();
-    
-        const formData : FormDataProps = {
-          name,
-          email,
-          message
-        };
-        if (!name || !email || !message  ){
-            toast.error("Some fields are missing")
-        }else{
-          toast.success("Message sent successfully")
-          const response = await axios.post(`/api`,formData);
-          setName("")
-          setEmail("")
-          setMessage("")
-        }
-      }
+  name: string;
+  email: string;
+  message: string;
+}
 
-    return ( 
-        <div className=" mt-16 w-full h-auto  flex lg:flex-row  sm:flex-col md:flex-col  justify-center sm:items-center md:items-center  lg:items-start gap-20   ">
-        {/* <div className="p1 lg:w-[30%] md:w-[80%] sm:w-[80%] flex flex-col gap-5  ">
+const WorkFrom = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const onSubmit = async (ev: React.FormEvent) => {
+    ev.preventDefault();
+
+    const formData: FormDataProps = {
+      name,
+      email,
+      message,
+    };
+    if (!name || !email || !message) {
+      toast.error("Some fields are missing");
+    } else {
+      toast.success("Message sent successfully");
+      const response = await axios.post(`/api`, formData);
+      setName("");
+      setEmail("");
+      setMessage("");
+    }
+  };
+
+  return (
+    <div className=" mt-16 w-full h-auto  flex lg:flex-row  sm:flex-col md:flex-col  justify-center sm:items-center md:items-center  lg:items-start gap-20   ">
+      {/* <div className="p1 lg:w-[30%] md:w-[80%] sm:w-[80%] flex flex-col gap-5  ">
             <h1 className="lg:text-6xl md:text-3xl  sm:text-3xl text-white">Let's work together!</h1>
             <p className="text-lg sm:text-base  text-white">Like my work and want something similar for your company? Sure, let's get to business!</p>
         </div> */}
-        <div className="lg:w-[50%] lg:h-[50%] md:w-[60%] md:h-[60%] sm:w-[80%] sm:h-[100%]     ">
-            <form onSubmit={onSubmit} className="w-full  h-full   lg:grid lg:grid-cols-2 md:flex md:flex-col  sm:flex sm:flex-col   gap-10 ">
-                <div className="flex flex-col  justify-center mx-4 ">
-                    <span className="text-white text-lg text-opacity-70">Your Name</span >
-                    <input type="text" className="bg-transparent text-purple-500  border-b borderwhite border-opacity-70 p-4 outline-none placeholder-white placeholder:opacity-40   " onChange={(ev)=>setName(ev.target.value)} value={name}  placeholder="Enter your name" />
-                </div>
+      <div className="lg:w-[50%] lg:h-[50%] md:w-[60%] md:h-[60%] sm:w-[80%] sm:h-[100%]     ">
+        <form
+          onSubmit={onSubmit}
+          className="w-full  h-full   lg:grid lg:grid-cols-2 md:flex md:flex-col  sm:flex sm:flex-col   gap-10 "
+        >
+          <div className="flex flex-col  justify-center mx-4 ">
+            <span className="text-white text-lg text-opacity-70">
+              Your Name
+            </span>
+            <input
+              type="text"
+              className="bg-transparent text-purple-500  border-b border-purple-500 border-opacity-70 p-4 outline-none placeholder-white placeholder:opacity-40   "
+              onChange={(ev) => setName(ev.target.value)}
+              value={name}
+              placeholder="Enter your name"
+            />
+          </div>
 
-                <div className="flex flex-col justify-center  mx-4" >
-                    <span className="text-white text-lg text-opacity-70">Email Address</span>
-                    <input type="text" className="bg-transparent text-purple-500  border-b borderwhite border-opacity-70 p-4 outline-none placeholder-white placeholder:opacity-40    "   onChange={(ev)=>setEmail(ev.target.value)} value={email} placeholder="Enter your email address" />
-                </div>
-                <div className="flex flex-col justify-center    lg:col-span-2 mx-4" >
-                    <span className="text-white text-lg text-opacity-70">Your Message</span>
-                    <input type="text" className="bg-transparent text-purple-500 border-b borderwhite border-opacity-70 p-4 outline-none placeholder-white placeholder:opacity-40   " onChange={(ev)=>setMessage(ev.target.value)} value={message}  placeholder="Hi, I think we need an app  for our products at Company X. How soon can you hop on to discuss this?" />
-                </div>
-               
-                <div className="flex flex-col justify-center  col-span-2 mx-4" >
-                <button 
-                      className="lg:w-[30%] md:w-[50%] sm:w-[80%] border p-2 m-auto text-white border-bl  sm:text-xs flex items-center justify-center gap-2 relative group   "
-                      >
-                              <div className="absolute inset-0 transition-all font-medium bg-white origin-left transform scale-x-0 group-hover:scale-x-100 duration-300"></div>
-                             <div className="hover:text-black hover:font-medium z-50 w-full h-full p-4 sm:p-2  flex items-center justify-center gap-2" >SHOOT</div>
-                  </button>
-                </div>
-            </form>
-        </div>
-      </div>  
-     );
-}
- 
+          <div className="flex flex-col justify-center  mx-4">
+            <span className="text-white text-lg text-opacity-70">
+              Email Address
+            </span>
+            <input
+              type="text"
+              className="bg-transparent text-purple-500  border-b border-purple-500 border-opacity-70 p-4 outline-none placeholder-white placeholder:opacity-40    "
+              onChange={(ev) => setEmail(ev.target.value)}
+              value={email}
+              placeholder="Enter your email address"
+            />
+          </div>
+          <div className="flex flex-col justify-center    lg:col-span-2 mx-4">
+            <span className="text-white text-lg text-opacity-70">
+              Your Message
+            </span>
+            <input
+              type="text"
+              className="bg-transparent text-purple-500 border-b border-purple-500 border-opacity-70 p-4 outline-none placeholder-white placeholder:opacity-40   "
+              onChange={(ev) => setMessage(ev.target.value)}
+              value={message}
+              placeholder="Hi, I think we need an app  for our products at Company X. How soon can you hop on to discuss this?"
+            />
+          </div>
+
+          <div className="flex flex-col justify-center   col-span-2 mx-4">
+            <button className="lg:w-[30%] md:w-[50%] text-white sm:w-[80%] border bg-transparent m-auto  border-purple-700  sm:text-xs flex items-center justify-center gap-2 relative group   ">
+              <div className="absolute inset-0 transition-all font-medium bg-purple-700 origin-left transform scale-x-0 group-hover:scale-x-100 duration-300"></div>
+              <div className="hover:text-white   py-6 hover:font-medium bg-transparent  z-50 w-full h-full p-4 sm:p-2  flex items-center justify-center gap-2">
+                SHOOT <ArrowRight />
+              </div>
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
 export default WorkFrom;
