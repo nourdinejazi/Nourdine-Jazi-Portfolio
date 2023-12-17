0bfbf4f7d630e88151dd668042b298e330825060
nourdine jazi <nourdinejazi2@gmail.com>
2023-12-17 17:24:52 +0100

    added projects link

--- app/projects/clothify-dashboard/tab-prev.tsx
+++ app/projects/clothify-dashboard/tab-prev.tsx
@@ -1,29 +1,71 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SimpleSlider from "./carousel";
 import Link from "next/link";
 import Image from "next/image";
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, Link2Icon } from "lucide-react";
 
 const TabPrev = () => {
-    return ( 
-<Tabs defaultValue="Preview" className="w-full  h-full  px-4  ">
-  <TabsList >
-    <TabsTrigger value="Preview">Preview</TabsTrigger>
-    <TabsTrigger value="Description">Description</TabsTrigger>
-  </TabsList>
-  <TabsContent className="  " value="Preview">
-      <div className=" w-full flex items-center justify-center my-4">
-            <span className="text-purple-500  flex items-center justify-center gap-2">Scroll Down <ArrowDown size={20} /> </span>
-      </div>
-      <SimpleSlider />
-  </TabsContent>
-  <TabsContent className=" text-slate-300 h-full " value="Description">
-    <div className="w-full h-full  mt-16  flex items-start justify-center   ">
-        <p className="lg:max-w-[1000px]  md:w-[90%]  sm:w-full text-lg p-2 sm:text-base  ">Meticulously crafted for seamless management of an online clothing store  <Link className="text-purple-500 underline" href="https://clothify-store.vercel.app/" target="_blanc"  >clothify-store.app</Link>  . Developed with <span className="text-purple-500">Next.js, React, and TypeScript</span>, the platform marries the power of dynamic frontend components with rock-solid type checking, ensuring a robust and intuitive user experience. <span className="text-purple-500">Tailwind CSS</span> lends its aesthetic prowess, delivering a sleek and responsive design that adapts flawlessly to any screen size. Beneath the surface, the project harnesses the prowess of <span className="text-purple-500">PostgreSQL and Prisma</span> , guaranteeing a reliable and performant database architecture that scales effortlessly. With <span className="text-purple-500">Clerk</span>  integration, user authentication becomes a breeze, enhancing security and personalization <Link className="text-purple-500 underline" href="https://clothify-admin.vercel.app/"  >(clothify-admin.app)</Link>.</p>
-    </div>
-  </TabsContent>
-</Tabs>
-     );
-}
- 
+  return (
+    <Tabs defaultValue="Preview" className="w-full  h-full  px-4  ">
+      <TabsList>
+        <TabsTrigger value="Preview">Preview</TabsTrigger>
+        <TabsTrigger value="Description">Description</TabsTrigger>
+      </TabsList>
+      <TabsContent className="  " value="Preview">
+        <div className=" w-full flex items-center justify-center my-4">
+          <span className="text-purple-500  flex items-center justify-center gap-2">
+            Scroll Down <ArrowDown size={20} />{" "}
+            <Link
+              className="text-bl underline text-white flex gap-2 items-center justify-center "
+              href="https://clothify-admin.vercel.app/"
+              target="_"
+            >
+              clothify-admin.app <Link2Icon></Link2Icon>
+            </Link>
+          </span>
+        </div>
+        <SimpleSlider />
+      </TabsContent>
+      <TabsContent className=" text-slate-300 h-full " value="Description">
+        <div className="w-full h-full  mt-16  flex items-start justify-center   ">
+          <p className="lg:max-w-[1000px]  md:w-[90%]  sm:w-full text-lg p-2 sm:text-base  ">
+            Meticulously crafted for seamless management of an online clothing
+            store{" "}
+            <Link
+              className="text-purple-500 underline"
+              href="https://clothify-store.vercel.app/"
+              target="_blanc"
+            >
+              clothify-store.app
+            </Link>{" "}
+            . Developed with{" "}
+            <span className="text-purple-500">
+              Next.js, React, and TypeScript
+            </span>
+            , the platform marries the power of dynamic frontend components with
+            rock-solid type checking, ensuring a robust and intuitive user
+            experience. <span className="text-purple-500">Tailwind CSS</span>{" "}
+            lends its aesthetic prowess, delivering a sleek and responsive
+            design that adapts flawlessly to any screen size. Beneath the
+            surface, the project harnesses the prowess of{" "}
+            <span className="text-purple-500">PostgreSQL and Prisma</span> ,
+            guaranteeing a reliable and performant database architecture that
+            scales effortlessly. With{" "}
+            <span className="text-purple-500">Clerk</span> integration, user
+            authentication becomes a breeze, enhancing security and
+            personalization{" "}
+            <Link
+              className="text-purple-500 underline"
+              href="https://clothify-admin.vercel.app/"
+            >
+              (clothify-admin.app)
+            </Link>
+            .
+          </p>
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
 export default TabPrev;
--- app/projects/clothify/tab-prev.tsx
+++ app/projects/clothify/tab-prev.tsx
@@ -1,34 +1,62 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SimpleSlider from "./carousel";
 import Link from "next/link";
 import Image from "next/image";
-import long_cap_clothify_big from '/public/long_cap_clothify_big.png'
-import { ArrowDown, ArrowRight } from "lucide-react";
+import long_cap_clothify_big from "/public/long_cap_clothify_big.png";
+import { ArrowDown, ArrowRight, Link2Icon } from "lucide-react";
 
-
-export const revalidate = 0
+export const revalidate = 0;
 
 const TabPrev = () => {
-    return ( 
-<Tabs defaultValue="Preview" className="w-full  h-full  px-4  ">
-  <TabsList >
-    <TabsTrigger value="Preview">Preview</TabsTrigger>
-    <TabsTrigger value="Description">Description</TabsTrigger>
-  </TabsList>
-  <TabsContent className="" value="Preview">
-      <div className=" w-full flex items-center justify-center my-4">
-          <span className="text-purple-500  flex items-center justify-center gap-2">Scroll Down <ArrowDown size={20} /> </span>
-      </div>
-      <SimpleSlider />
-  </TabsContent>
-  <TabsContent className=" text-slate-300 h-full " value="Description">
-    <div className="w-full h-full  mt-16  flex items-start justify-center   ">
-        <p className="lg:max-w-[1000px]  md:w-[90%]  sm:w-full text-lg p-2 sm:text-base  ">• Clothify, sophisticated clothing website meticulously crafted mainly  using <span className="text-bl">Next.js, React, and Tailwind CSS</span>. Seamlessly connected to an intuitive <Link className="text-bl underline"  href={'/work/clothify-admin' } target="_">admin interface</Link> through <span className="text-bl  ">APIs</span> <Link className="text-bl underline text-purple-500" href="https://clothify-store.vercel.app/" target="_" >Visit live :(clothify-store.app)</Link>.</p>
-        
-    </div>
-  </TabsContent>
-</Tabs>
-     );
-}
- 
+  return (
+    <Tabs defaultValue="Preview" className="w-full  h-full  px-4  ">
+      <TabsList>
+        <TabsTrigger value="Preview">Preview</TabsTrigger>
+        <TabsTrigger value="Description">Description</TabsTrigger>
+      </TabsList>
+      <TabsContent className="" value="Preview">
+        <div className=" w-full flex items-center justify-center my-4">
+          <span className="text-purple-500  flex items-center justify-center gap-2">
+            Scroll Down <ArrowDown size={20} />
+            <Link
+              className="text-bl underline text-white flex gap-2 items-center justify-center "
+              href="https://clothify-store.vercel.app/"
+              target="_"
+            >
+              clothify-store.app <Link2Icon></Link2Icon>
+            </Link>
+          </span>
+        </div>
+        <SimpleSlider />
+      </TabsContent>
+      <TabsContent className=" text-slate-300 h-full " value="Description">
+        <div className="w-full h-full  mt-16  flex items-start justify-center   ">
+          <p className="lg:max-w-[1000px]  md:w-[90%]  sm:w-full text-lg p-2 sm:text-base  ">
+            • Clothify, sophisticated clothing website meticulously crafted
+            mainly using{" "}
+            <span className="text-bl">Next.js, React, and Tailwind CSS</span>.
+            Seamlessly connected to an intuitive{" "}
+            <Link
+              className="text-bl underline"
+              href={"/work/clothify-admin"}
+              target="_"
+            >
+              admin interface
+            </Link>{" "}
+            through <span className="text-bl  ">APIs</span>{" "}
+            <Link
+              className="text-bl underline text-purple-500"
+              href="https://clothify-store.vercel.app/"
+              target="_"
+            >
+              Visit live :(clothify-store.app)
+            </Link>
+            .
+          </p>
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
+};
+
 export default TabPrev;
