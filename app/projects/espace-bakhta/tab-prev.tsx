8748d07d637e181d77a7b0ac42ca96d44fa9446c
nourdine jazi <nourdinejazi2@gmail.com>
2023-12-17 17:24:52 +0100

    added projects link

--- app/projects/espace-bakhta/tab-prev.tsx
+++ app/projects/espace-bakhta/tab-prev.tsx
@@ -1,30 +1,51 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import SimpleSlider from "./carousel";
 import Link from "next/link";
 import Image from "next/image";
-import { ArrowDown } from "lucide-react";
+import { ArrowDown, Link2Icon } from "lucide-react";
 
 const TabPrev = () => {
-    return ( 
-<Tabs defaultValue="Preview" className="w-full  h-full  px-4  ">
-  <TabsList >
-    <TabsTrigger value="Preview">Preview</TabsTrigger>
-    <TabsTrigger value="Description">Description</TabsTrigger>
-  </TabsList>
-  <TabsContent className="  " value="Preview">
-      <div className=" w-full flex items-center justify-center my-4">
-            <span className="text-purple-500  flex items-center justify-center gap-2">Scroll Down <ArrowDown size={20} /> </span>
-      </div>
-      <SimpleSlider />
-  </TabsContent>
-  <TabsContent className=" text-slate-300 h-full " value="Description">
-    <div className="w-full h-full  mt-16  flex items-start justify-center   ">
-        <p className="lg:max-w-[1000px]  md:w-[90%]  sm:w-full text-lg p-2 sm:text-base ">Crafted with <span className="text-purple-500">Next js and Tailwind CSS</span>, the Espace Bakhta website embodies seamless navigation and elegant design. Tailwind CSS adds a touch of finesse with responsive layouts.  <Link className="text-purple-500" href="https://www.espace-bakhta.tn/"  >(www.espace-bakhta.tn)</Link>.</p>
+  return (
+    <Tabs defaultValue="Preview" className="w-full  h-full  px-4  ">
+      <TabsList>
+        <TabsTrigger value="Preview">Preview</TabsTrigger>
+        <TabsTrigger value="Description">Description</TabsTrigger>
+      </TabsList>
+      <TabsContent className="  " value="Preview">
+        <div className=" w-full flex items-center justify-center my-4">
+          <span className="text-purple-500  flex items-center justify-center gap-2">
+            Scroll Down <ArrowDown size={20} />{" "}
+            <Link
+              className="text-bl underline text-white flex gap-2 items-center justify-center "
+              href="https://espace-bakhta.tn/"
+              target="_"
+            >
+              espace-bakhta.tn <Link2Icon></Link2Icon>
+            </Link>{" "}
+          </span>
+        </div>
+        <SimpleSlider />
+      </TabsContent>
+      <TabsContent className=" text-slate-300 h-full " value="Description">
+        <div className="w-full h-full  mt-16  flex items-start justify-center   ">
+          <p className="lg:max-w-[1000px]  md:w-[90%]  sm:w-full text-lg p-2 sm:text-base ">
+            Crafted with{" "}
+            <span className="text-purple-500">Next js and Tailwind CSS</span>,
+            the Espace Bakhta website embodies seamless navigation and elegant
+            design. Tailwind CSS adds a touch of finesse with responsive
+            layouts.{" "}
+            <Link
+              className="text-purple-500"
+              href="https://www.espace-bakhta.tn/"
+            >
+              (www.espace-bakhta.tn)
+            </Link>
+            .
+          </p>
+        </div>
+      </TabsContent>
+    </Tabs>
+  );
+};
 
-    </div>
-  </TabsContent>
-</Tabs>
-     );
-}
- 
 export default TabPrev;
